7672ba18c3c6871fac7771d2c711ec321467bdfc
Yarha <61147973+Yarha@users.noreply.github.com>
2020-02-27 20:58:58 +0100

    finished assignment

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/38b22144-aa4a-4e51-4840-949cce028fd8-4e83/",
                 "target": "_blank"
             }
         ]
